023b53c66d56013a6e6d805e14c64efc1324febb
XeniaBuerki <xenia1898@gmail.com>
2020-04-11 21:02:08 +0700

    Add first Controller

--- src/Controller/QuestionsController.php
+++ src/Controller/QuestionsController.php
@@ -4,12 +4,27 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionsController
+class QuestionsController extends AbstractController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage()
     {
         return new Response('What a bewitching controller we have conjured!');
+    }
+    
+    /**
+     * @Route("/questions/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf('My own question "%s"',
+        ucwords(str_replace("-", " ", $slug))
+        ));
     }
 }
